b070a81cf9e1fa01599c0f872163a6d0855fdb91
Thiagojm <tjm.plastica@gmai.com>
2021-01-25 22:08:50 -0300

    Beep working Icon added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import random
 from datetime import datetime
 import time
+import winsound
 
 # External Imports
 import PySimpleGUI as sg
@@ -25,11 +26,11 @@ def trng3_random():
         ser.setDTR(True)
         ser.flushInput()
     except Exception:
-        sg.popupmsg("Warning!", f"Port Not Usable! Do you have permissions set to read {rng_com_port}?")
+        return
     try:
         x = ser.read(blocksize)  # read bytes from serial port
     except Exception:
-        sg.popupmsg("Warning!", "Read failed!")
+        return
     ser.close()
     bin_hex = BitArray(x)  # bin to hex
     bin_ascii = bin_hex.bin  # hex to ASCII
@@ -47,8 +48,8 @@ def trng3_random():
 
 def main():
     events_list = ("a", "s", "d", "f")
-    events_dict = {"a": "Vermelho", "s": "Amarelo", "d": "Azul", "f": "Verde", " ": "src/white.png"}
-    image_dict = {"Vermelho": "src/red.png", "Amarelo": "src/yellow.png", "Azul": "src/blue.png", "Verde": "src/green.png"}
+    events_dict = {"a": "Vermelho", "s": "Amarelo", "d": "Azul", "f": "Verde"}
+    image_dict = {"Vermelho": "src/images/red.png", "Amarelo": "src/images/yellow.png", "Azul": "src/images/blue.png", "Verde": "src/images/green.png"}
     count = 0
     doc_name = None
     act_image = None
@@ -56,19 +57,23 @@ def main():
     right_hits = 0
     percent = 0.0
     ongoing = False
+    # Beep config
+    frequency = 500  # Set Frequency To 1000 Hertz
+    duration = 250  # Set Duration To 250 ms
 
-    layout = [[sg.Text("Nome do Usuário: "), sg.Listbox(values=('Mony', "Thi"), key="user_name", default_values='Mony', size=(18, 2)),
-               sg.Text("A = VERMELHO, S = AMARELO, D = AZUL, F = VERDE")],
-              [sg.Radio('Pseudo-Random', "RADIO1", default=True, k="pseudo"), sg.Radio('TrueRNG3', "RADIO1", k="trng")],
-              [sg.Image(filename="src/white.png", key="image")],
-              [sg.Text("Você escolheu a cor: "), sg.Text("", size=(18, 1), key='text'),
-               sg.Text("Número da rodada: "), sg.Text("0", size=(18, 1), key='COUNT'),
-               sg.Text("Acertos: "), sg.Text(f"{right_hits} ({percent}%)", size=(18, 1), key='HITS')],
-              [sg.Button("Start", key='START/STOP')],
-              [sg.Button("Exit", key='EXIT')]]
+    sg.theme('LightGreen')
+
+    layout = [[sg.Text("Nome do Usuário: "), sg.Listbox(values=('Mony', "Thi", "Outro"), key="user_name", default_values='Mony', size=(18, 2)),
+               sg.Radio('Pseudo-Random', "RADIO1", default=True, k="pseudo"), sg.Radio('TrueRNG3', "RADIO1", k="trng")],
+               [sg.Text("Legenda: "), sg.Text("A = VERMELHO, S = AMARELO, D = AZUL, F = VERDE")],
+              [sg.T(size=(7, 1)), sg.Image(filename="src/images/white.png", key="image")],
+              [sg.Text("Você escolheu a cor: "), sg.Text("", key='text', size=(9, 1)),
+               sg.Text("Número da rodada: "), sg.Text("0", key='COUNT', size=(3, 1)),
+               sg.Text("Acertos: "), sg.Text(f"{right_hits} ({percent}%)", key='HITS', size=(10, 1))],
+              [sg.T(size=(10, 1)), sg.Button("Start", key='START/STOP', size=(20, 1)), sg.Button("Exit", key='EXIT', size=(20, 1))]]
 
     window = sg.Window("InfoVision 1.0", layout,
-                       return_keyboard_events=True, use_default_focus=False, location=(300, 20))
+                       return_keyboard_events=True, use_default_focus=False, location=(300, 20), icon=("src/images/tapa_olho.ico"))
 
     # ---===--- Loop taking in user input --- #
     while True:
@@ -81,19 +86,36 @@ def main():
             print(event, "exiting")
             break
         if event == "START/STOP":
-            print(values)
             if not ongoing:
-                ongoing = True
-                doc_name = datetime.now().strftime(f"{values['user_name'][0]}_%Y_%m_%d-%H_%M_%S")
-                if values["pseudo"]:
-                    act_image = random.choice(list(image_dict.keys()))
-                else:
-                    act_image = trng3_random()
-                time.sleep(3)
-                img_element.update(image_dict[act_image])
-                count = 1
-                count_element.update(count)
-                window['START/STOP'].update("Stop")
+                try:
+                    ongoing = True
+                    doc_name = datetime.now().strftime(f"{values['user_name'][0]}_%Y_%m_%d-%H_%M_%S")
+                    sg.popup_no_buttons("Iniciando em 3 segundos, aguarde...", auto_close_duration=3, auto_close=True,
+                                        non_blocking=True, icon=("src/images/tapa_olho.ico"))
+                    time.sleep(3)
+                    if values["pseudo"]:
+                        act_image = random.choice(list(image_dict.keys()))
+                    else:
+                        act_image = trng3_random()
+                    winsound.Beep(frequency, duration)
+                    img_element.update(image_dict[act_image])
+                    count = 1
+                    count_element.update(count)
+                    window['START/STOP'].update("Stop")
+                except Exception:
+                    sg.popup_ok("Warning!", "Read failed! Try using Pseudo-Random mode.", icon=("src/images/tapa_olho.ico"))
+                    count = 0
+                    doc_name = None
+                    act_image = None
+                    hit = None
+                    right_hits = 0
+                    percent = 0.0
+                    img_element.update("src/images/white.png")
+                    text_elem.update(" ")
+                    percent_element.update(f"{right_hits} ({percent}%)")
+                    count_element.update(count)
+                    window['START/STOP'].update("Start")
+                    ongoing = False
             else:
                 count = 0
                 doc_name = None
@@ -101,33 +123,48 @@ def main():
                 hit = None
                 right_hits = 0
                 percent = 0.0
-                img_element.update("src/white.png")
+                img_element.update("src/images/white.png")
                 text_elem.update(" ")
                 percent_element.update(f"{right_hits} ({percent}%)")
                 count_element.update(count)
                 window['START/STOP'].update("Start")
                 ongoing = False
         if event in events_list:
-            if ongoing:
-                text_elem.update(events_dict[event])
-                if act_image == events_dict[event]:
-                    hit = True
-                    right_hits += 1
-                else:
-                    hit = False
-                percent = round(float(right_hits * 100 / count), 2)
-                with open(f'{doc_name}.csv', 'a+') as fd:
-                    fd.write(f"{count}, {act_image}, {events_dict[event]}, {hit}, {percent}\n")
-                percent = round(float(right_hits * 100/ count), 2)
+            try:
+                if ongoing:
+                    text_elem.update(events_dict[event])
+                    if act_image == events_dict[event]:
+                        hit = True
+                        right_hits += 1
+                    else:
+                        hit = False
+                    percent = round(float(right_hits * 100 / count), 2)
+                    with open(f'{doc_name}.csv', 'a+') as fd:
+                        fd.write(f"{count}, {act_image}, {events_dict[event]}, {hit}, {percent}\n")
+                    percent = round(float(right_hits * 100/ count), 2)
+                    percent_element.update(f"{right_hits} ({percent}%)")
+                    count += 1
+                    count_element.update(count)
+                    if values["pseudo"]:
+                        act_image = random.choice(list(image_dict.keys()))
+                    else:
+                        act_image = trng3_random()
+                    winsound.Beep(frequency, duration)
+                    img_element.update(image_dict[act_image])
+            except Exception:
+                sg.popup_ok("Warning!", "Read failed! Try using Pseudo-Random mode.", icon=("src/images/tapa_olho.ico"))
+                count = 0
+                doc_name = None
+                act_image = None
+                hit = None
+                right_hits = 0
+                percent = 0.0
+                img_element.update("src/images/white.png")
+                text_elem.update(" ")
                 percent_element.update(f"{right_hits} ({percent}%)")
-                count += 1
                 count_element.update(count)
-                if values["pseudo"]:
-                    act_image = random.choice(list(image_dict.keys()))
-                else:
-                    act_image = trng3_random()
-                img_element.update(image_dict[act_image])
-
+                window['START/STOP'].update("Start")
+                ongoing = False
 
     window.close()
 
---
+++ tests.py
@@ -0,0 +1,4 @@
+import winsound
+frequency = 500  # Set Frequency To 1000 Hertz
+duration = 250  # Set Duration To 250 ms
+winsound.Beep(frequency, duration)
