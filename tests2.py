439010f770a30a9bf438a0f7b2268155ecdc0b6d
Thiagojm <tjm.plastica@gmail.com>
2021-02-13 01:09:24 -0300

    up

--- tests2.py
+++ tests2.py
@@ -1,5 +1,15 @@
-import secrets
+import time
 
-x = secrets.token_bytes()
 
-print(x)
+def fib_func(n):
+    if n <2:
+        return n
+    return fib_func(n - 1) + fib_func(n - 2)
+
+
+# Driver Program
+
+t0 = time.time()
+print(fib_func(33))
+t1 = time.time()
+print(t1-t0)
